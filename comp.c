d8b7912c1be23064c4367bfaf16a1cd81e114f61
MorkiWay <105401252+MorkiWay@users.noreply.github.com>
2024-11-27 06:47:07 -0500

    Add files via upload
    I forgot the source code file again lmao

--- comp.c
+++ comp.c
@@ -10,223 +10,8 @@ struct function_node  {struct function fun; struct function_node* next; int find
 struct function_node* fun_list = NULL;
 struct function* var_list=NULL;
 
-void check_var_against_functions(char* var_name)
-{
-	struct function_node* temp = fun_list;
-	while (temp!=NULL)
-	{
-		if (strcmp(var_name, temp->fun.fun_name)==0)
-		{
-			printf("Variable %s has the name of a defined function\n",var_name);
-			exit(0);
-		}
-		temp=temp->next;
-	}
-
-};
-
-
-void free_vars(struct variable_node* vars)
-{
-	struct variable_node* temp=NULL;
-	
-	free(vars->var_name);
-	//printf("in free_vars\n");fflush(stdout);
-	while(vars->next!=NULL)
-	{
-		temp=vars->next;
-		//free(vars->var_name);
-		free(vars);
-		//printf("in loop\n");fflush(stdout);
-		vars=temp;
-		//printf("%s\n",vars->var_name);fflush(stdout);
-		free(vars->var_name);
-	}
-	free(vars);
-
-	//printf("out of loop\n"); fflush(stdout);
-};
-
-void free_function(struct function f)
-{
-	free(f.fun_name);
-	//printf("%s","HERE\n");fflush(stdout);
-	if(f.vars!=NULL)
-		free_vars(f.vars);
-};
-
-
-int insert_var(struct function* f,char *n, char t, int o, int c)
-{
-	f->args++;
-		
-	for (int p=o+1; p<=c; p++)
-	{
-		struct ast* temp_node=find_ast_node(p);
-		if (strcmp(n, temp_node->token)==0)
-		{
-			//printf("FOUND: %s\n", temp_node->token);
-			//printf("WAS: %c",temp_node->type);
-			temp_node->type = t;
-			//printf("NOW: %c",temp_node->type);
-		}
-	}
-
-
-	check_var_against_functions(n);
-
-	struct variable_node* new_node = malloc (sizeof(struct variable_node));
-
-		new_node->var_name=malloc(sizeof(char)*(strlen(n)+1));
-		strcpy(new_node->var_name,n);
-		new_node->type=t;
-		new_node->next=NULL;
-		new_node->open=o;
-		new_node->close=c;
-	
-	struct variable_node* temp = f->vars;
-	
-	if (f->vars==NULL)
-	{
-		f->vars=new_node;
-	}
-	else if (f->vars->next==NULL)
-	{
-		if (strcmp(f->vars->var_name,n)==0 && !(f->vars->close < o || c < f->vars->open))
-			{printf("Variable %s is declared twice\n",n); exit(0);}
-		else
-			f->vars->next=new_node;
-
-	//	printf("3\n");fflush(stdout);
-	}
-	else
-	{
-		while (temp->next!=NULL)
-		{
-			//printf("CURRENT NODE AGAIN: %s\n",temp->var_name);
-			if (strcmp(temp->next->var_name,n)==0 && !( temp->next->close < o || c < temp->next->open) )
-				{printf("Variable %s is defined twice\n",n); exit(0);}
-			temp=temp->next;
-		}
-		temp->next=new_node;
-	}
-
-	return 0;
-};
-
-struct function_node* insert_function(struct function_node* f, char* n, char t)
-{
-
-	struct function_node* new_function=malloc(sizeof(struct function_node));
-
-	new_function->fun.fun_name = malloc(sizeof(char)*(strlen(n)+1));
-	strcpy(new_function->fun.fun_name,n);
-	new_function->next=NULL;
-	new_function->fun.return_type=t;
-	new_function->fun.args=0;
-	new_function->fun.vars=NULL;
-	new_function->finds=0;
-
-	struct ast* temp_node = find_ast_node(1);
-
-	for (int q=2; temp_node!=NULL; q++)
-	{
-		if (strcmp(n, temp_node->token)==0)
-		{
-			//printf("FOUND: %s\n", temp_node->token);
-			temp_node->type = t;
-		}
-		temp_node=find_ast_node(q);
-	}
-
-	struct function_node* temp = f;
-
-	//printf("inserting_fun\n"); fflush(stdout);
-	if (f==NULL) 
-	{
-		//printf("FIRST FUN\n");fflush(stdout);
-		return new_function;
-	}
-	else if(f->next==NULL)
-	{
-		if(  strcmp(f->fun.fun_name,n)!=0 )
-			f->next=new_function;
-		else
-			{printf("Function %s is defined twice\n",n); exit(0);}
-	}
-	else
-	{
-		while(temp->next!=NULL)
-		{
-			if(strcmp(n,temp->fun.fun_name)==0)
-				{printf("Function %s is defined twice\n",n); exit(0);}
-			temp=temp->next;
-		}
-		
-		temp->next=new_function;
-	}
-	return f;
-};
-
-/*
-void free_fun_list(struct function_node* N)
-{
-	struct function_node* temp=N;
-
-	while(N!=NULL)
-	{
-		free(N->fun.fun_name);
-		free(N
-	}
-}*/
-
-void print_function_table(struct function_node* list)
-{
-	struct function_node* temp=list;
-
-	while(temp!=NULL)
-	{
-		printf("FUN: %s\tTYPE: %c\tNUM_ARGS: %d\tARGS: ",temp->fun.fun_name,temp->fun.return_type,temp->fun.args);
-		struct variable_node* temp2 = temp->fun.vars;
-		while(temp2!=NULL)
-		{
-			printf("%c %s\t",temp2->type,temp2->var_name);
-			temp2=temp2->next;
-		}
-		printf("\n");
-
-		temp=temp->next;
-	}
-};
-
-void print_var_list(struct function* var_list)
-{
-	struct variable_node* temp=var_list->vars;
-	while (temp!=NULL)
-	{
-		printf("VAR: %s\tTYPE: %c\tOPEN: %d\tCLOSE: %d\n",temp->var_name,temp->type,temp->open,temp->close);
-		temp=temp->next;
-	}
-}
-
-char resolve_let(struct ast* node)
-{
-	//printf("RESOLVING NODE: %d \t%s\n",node->id,node->token); fflush(stdout);
-	//printf("CHILDREN %s %s %s\n",get_child(node,1)->token,get_child(node,2)->token,get_child(node,3)->token);
-	node->type=get_child(node,3)->type;
-	return node->type;
-}
-
-char resolve_if(struct ast* node)
-{
-	//printf("at: %s\t child1: %s\t child2: %s\n",node->token,get_child(node,2)->token,get_child(node,3)->token);
-	if(get_child(node,2)->type==get_child(node,3)->type && get_child(node,1)->type=='b')
-		node->type=get_child(node,2)->type;
-	else
-		{printf("Arguements of if do not type check\n"); exit(0);}
-	
-	return node->type;
-}
+#include "semantics_utils.h"
+#include "cfg.h"
 
 
 int main (int argc, char **argv) {
@@ -248,7 +33,6 @@ int main (int argc, char **argv) {
 	
 	struct ast* node=find_ast_node(1);
 
-
 	//add all functions to the function table
 	for(int i =2; node!=NULL; i++)
 	{
@@ -266,6 +50,7 @@ int main (int argc, char **argv) {
 		node=find_ast_node(i);
 	}
 
+	int size_ast=0;
 	//fill in function variables
 	node=find_ast_node(1);
 	for (int i=2; node!=NULL; i++)
@@ -311,43 +96,65 @@ int main (int argc, char **argv) {
 
 		}
 		node=find_ast_node(i);
+		size_ast++;
 	}
 
-
-
-	//fill in other variables, even if type not yet known 
-	node=find_ast_node(1);
-	for (int i=2; node!=NULL; i++)
+	//printf("SIZE OF AST: %d\n",size_ast);
+	for (int i = size_ast; i>0; i--)
 	{
-		//find vars
-		if (strcmp(node->token,"let")==0) //vardec
+		node=find_ast_node(i);
+		if(strcmp(node->token,"let")==0)
 		{
-
-			//resolve_let(node);
+			//printf("FOUND: %d\n",i);
+	
 			int kids=get_child_num(node);
 			
 			get_child(node,1)->type=get_child(node,2)->type;			
 
 			insert_var(var_list,get_child(node,1)->token,get_child(node,1)->type,get_child(node,1)->id,get_child(node,kids)->id);
+		}
+	}
+
+
+	//printf("filling in other variables\n");fflush(stdout);
+	//fill in other variables, even if type not yet known 
+	node=find_ast_node(1);
+	for (int i=2; node!=NULL; i++)
+	{
+		//printf("VALUE: %s\n",node->token); fflush(stdout);
+		//find vars
+		if (strcmp(node->token,"let")==0) //vardec
+		{
+			//printf("VAR IDENTIFIED\n");
+
+			//resolve_let(node);
 			resolve_let(node);
+
+			//printf("AFTER INSERTION:\n");
+			//print_var_list(var_list);
+			//printf("EOL\n");
 
 		}
 		if(node->type=='c') // if statement
 		{
 			resolve_if(node);
-		}
+		}/*
 		if(node->type=='f')	// func_def
 		{
+			//printf("HERE\n"); fflush(stdout);
 			int kids=get_child_num(node);
-			if (get_child(node,kids)->type!=get_child(node,kids-1)->type)
-				{printf("result of %s doesnt match return type\n",get_child(node,1)->token); exit(0);}
-
+			//if (kids>0)
+			//{
+				if (get_child(node,kids)->type!=get_child(node,kids-1)->type)
+					{printf("result of %s doesnt match return type\n",get_child(node,1)->token); exit(0);}
+			//}
 		}
+		//printf("RESOLVED\n");fflush(stdout);*/
 		
 		node=find_ast_node(i);
 	}
 
-
+	//printf("solving last types\n"); fflush(stdout);
 	//solve types of whole tree
 	//int finds=0;
 	node=find_ast_node(1);
@@ -363,7 +170,7 @@ int main (int argc, char **argv) {
 			{					
 				if (get_child(node,p)->type!='i')
 				{
-					printf("Arguments of %s do not type check\n",node->token); exit(0);
+					printf("1 Arguments of %s do not type check\n",node->token); exit(0);
 				}
 			}
 		}
@@ -375,7 +182,7 @@ int main (int argc, char **argv) {
 			{
 				if (get_child(node,p)->type!='b')
 				{
-					printf("Arguemnts of %s do not type check\n",node->token); exit(0);
+					printf("2 Arguemnts of %s do not type check\n",node->token); exit(0);
 				}
 			}
 		}
@@ -415,12 +222,25 @@ int main (int argc, char **argv) {
 
 			temp_func_node=temp_func_node->next;
 		}
+
+
+		//print_var_list(var_list); 
+
+
 		if (node->type=='f')
 			{printf("Function %s is not defined\n",node->token); exit(0);}
 
 		
 		if (node->type=='v')
 			{printf("Variable %s is not declared\n",node->token); exit(0);}
+
+	
+		if (strcmp(node->token,"DEFINE-FUN")==0)	
+		{	int kids=get_child_num(node);
+				if (get_child(node,kids)->type!=get_child(node,kids-1)->type)
+					{printf("Return type of %s does not match the type of body\n",get_child(node,1)->token); exit(0);}
+		}
+
 	
 		//printf("NODE: %d\t TYPE: %c\t NAME: %s\n",node->id,node->type,node->token);
 		node=find_ast_node(i);
@@ -431,7 +251,65 @@ int main (int argc, char **argv) {
 	//print_function_table(fun_list);
 	//print_var_list(var_list);
 
+	
+	cfp= fopen("cfg.dot", "w");
+ 	fprintf(cfp, "digraph print {\n");
+	
+	node=find_ast_node(1);
+	
+	while (node!=NULL)
+	{
+		if (strcmp(node->token,"DEFINE-FUN")==0 || strcmp(node->token,"ENTRY")==0)
+		//then it needs a cfg 
+		{
+			//printf("\nCFG FOR %s\n\n",node->token);
+
+
+			if (get_child_num(node)>2)
+			{
+				struct ast* base_node=find_ast_node((get_child(node,get_child_num(node)-1)->id)+0);
+
+				int offset=look_ahead(base_node);
+				//printf("OFFSET: %d\n",offset);
+
+
+
+
+				//printf("%s POINTS to %d\n",get_child(node,1)->token,find_ast_node(base_node->id+offset)->id);
+				fprintf(cfp,"%d [label=\"%s\"];\n",get_child(node,1)->id,get_child(node,1)->token);
+				fprintf(cfp,"%d->%d\n",get_child(node,1)->id,find_ast_node(base_node->id+offset)->id);
+
+				say_and_point_to(find_ast_node(base_node->id+offset),get_child(node,get_child_num(node))->id);
+			}
+			else
+			{
+				struct ast* base_node=find_ast_node((get_child(node,1)->id)+0);
+
+				int offset=look_ahead(base_node);
+				//printf("OFFSET: %d\n",offset);
+
+
+
+				//printf("%s POINTS to %d\n",get_child(node,1)->token,find_ast_node(base_node->id+offset)->id); //name of starting node
+				fprintf(cfp,"%d [label=\"%s\"];\n",get_child(node,1)->id,get_child(node,1)->token);
+				fprintf(cfp,"%d->%d\n",get_child(node,1)->id,find_ast_node(base_node->id+offset)->id);
+				say_and_point_to(find_ast_node(base_node->id+offset),node->id-1);
+			}
+
+		}
+		node=node->next;
+	}
+
+
+ fprintf(cfp, "}\n ");
+ fclose(cfp);
+ system("dot -Tpdf cfg.dot -o cfg.pdf");
+
   free_ast();
   return retval;
+
+
+
+
 };
 
